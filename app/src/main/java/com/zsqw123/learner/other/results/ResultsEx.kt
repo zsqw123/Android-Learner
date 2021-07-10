54069cfaf5df89e93de19f86d93834f1d4e240a7
damyjy <zsqw123@vip.qq.com>
2021-07-10 23:14:18 +0800

    - add more Activity Results features

--- app/src/main/java/com/zsqw123/learner/other/results/ResultsEx.kt
+++ app/src/main/java/com/zsqw123/learner/other/results/ResultsEx.kt
@@ -5,8 +5,10 @@ import android.graphics.Bitmap
 import android.net.Uri
 import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCaller
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
 
 /**
  * Author zsqw123
@@ -14,37 +16,40 @@ import androidx.core.os.bundleOf
  * Date 2021/7/10 13:38
  */
 
-fun ComponentActivity.takePhoto(targetUri: Uri, callback: (Boolean) -> Unit) =
+fun ActivityResultCaller.takePhoto(targetUri: Uri, callback: (Boolean) -> Unit) =
     registerForActivityResult(ActivityResultContracts.TakePicture(), callback).launch(targetUri)
 
-fun ComponentActivity.takePhoto(callback: (Bitmap) -> Unit) =
+fun ActivityResultCaller.takePhoto(callback: (Bitmap) -> Unit) =
     registerForActivityResult(ActivityResultContracts.TakePicturePreview(), callback).launch(null)
 
-fun ComponentActivity.requestPermissions(vararg permissions: String, callback: (Map<String, Boolean>) -> Unit) =
+fun ActivityResultCaller.requestPermissions(vararg permissions: String, callback: (Map<String, Boolean>) -> Unit) =
     registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions(), callback).launch(permissions)
 
-fun ComponentActivity.takeVideo(targetUri: Uri, callback: (Bitmap) -> Unit) =
+fun ActivityResultCaller.takeVideo(targetUri: Uri, callback: (Bitmap) -> Unit) =
     registerForActivityResult(ActivityResultContracts.TakeVideo(), callback).launch(targetUri)
 
-fun ComponentActivity.pickContact(callback: (Uri) -> Unit) =
+fun ActivityResultCaller.pickContact(callback: (Uri) -> Unit) =
     registerForActivityResult(ActivityResultContracts.PickContact(), callback).launch(null)
 
-fun ComponentActivity.getContent(mimeType: String, callback: (Uri) -> Unit) =
+fun ActivityResultCaller.getContent(mimeType: String, callback: (Uri) -> Unit) =
     registerForActivityResult(ActivityResultContracts.GetContent(), callback).launch(mimeType)
 
-fun ComponentActivity.createDocument(defaultName: String, callback: (Uri) -> Unit) =
+fun ActivityResultCaller.createDocument(defaultName: String, callback: (Uri) -> Unit) =
     registerForActivityResult(ActivityResultContracts.CreateDocument(), callback).launch(defaultName)
 
-fun ComponentActivity.openDocuments(vararg mimeType: String, callback: (List<Uri>) -> Unit) =
+fun ActivityResultCaller.openDocuments(vararg mimeType: String, callback: (List<Uri>) -> Unit) =
     registerForActivityResult(ActivityResultContracts.OpenMultipleDocuments(), callback).launch(mimeType)
 
-fun ComponentActivity.openDocument(vararg mimeType: String, callback: (Uri) -> Unit) =
+fun ActivityResultCaller.openDocument(vararg mimeType: String, callback: (Uri) -> Unit) =
     registerForActivityResult(ActivityResultContracts.OpenDocument(), callback).launch(mimeType)
 
-fun ComponentActivity.openDocumentTree(defaultUri: Uri? = null, callback: (Uri) -> Unit) =
+fun ActivityResultCaller.openDocumentTree(defaultUri: Uri? = null, callback: (Uri) -> Unit) =
     registerForActivityResult(ActivityResultContracts.OpenDocumentTree(), callback).launch(defaultUri)
 
-inline fun <reified C> ComponentActivity.startAndResult(noinline callback: (ActivityResult) -> Unit, vararg pairs: Pair<String, Any>) =
+inline fun <reified C> ComponentActivity.startAndResult(vararg pairs: Pair<String, Any>, noinline callback: (ActivityResult) -> Unit) =
     registerForActivityResult(ActivityResultContracts.StartActivityForResult(), callback)
         .launch(Intent(this, C::class.java).apply { putExtras(bundleOf(*pairs)) })
 
+inline fun <reified C> Fragment.startAndResult(vararg pairs: Pair<String, Any>, noinline callback: (ActivityResult) -> Unit) =
+    registerForActivityResult(ActivityResultContracts.StartActivityForResult(), callback)
+        .launch(Intent(context, C::class.java).apply { putExtras(bundleOf(*pairs)) })
